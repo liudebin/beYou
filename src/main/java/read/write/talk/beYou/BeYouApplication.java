51232febfecef0c6290da6e756598d8df9b496dc
unknown <849470446@qq.om>
2020-01-18 12:11:04 +0800

    init

---
+++ src/main/java/read/write/talk/beYou/BeYouApplication.java
@@ -0,0 +1,13 @@
+package read.write.talk.beYou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeYouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeYouApplication.class, args);
+	}
+
+}
---
+++ src/test/java/read/write/talk/beYou/BeYouApplicationTests.java
@@ -0,0 +1,13 @@
+package read.write.talk.beYou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeYouApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
